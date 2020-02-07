ba73ac15bd1c9bc17f337e577b7789a411fe17b0
Deepmala  Bhomi <bhomid@warhawks.ulm.edu>
2020-02-07 13:56:46 -0600

    fix dividing error

--- Test.java
+++ Test.java
@@ -4,10 +4,17 @@ public class Test
    {  
       System.out.println("Hello World");
       printer("hello world and universe");
+      
+      System.out.println(divider(10,3));
    }
    
-   public static void printer(String[] Phrase)
+   public static void printer(String phrase)
    {
       System.out.println(phrase);
+   }
+   
+   public static float divider(int no1, int no2)
+   {
+      return no1/no2;
    }
 }
